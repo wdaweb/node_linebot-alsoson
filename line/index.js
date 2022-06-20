d9336762d130bb8367c7cda5b378ea13b762371c
alsoson <0971579957lulu@gmail.com>
2022-06-20 20:36:10 +0800

    Add

---
+++ line/index.js
@@ -0,0 +1,30 @@
+import 'dotenv/config'
+import linebot from 'linebot'
+import schedule from 'node-schedule'
+import data from './data.js'
+
+data.fetchData()
+
+schedule.scheduleJob('0 0 * * *', () => {
+  data.fetchData()
+})
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+bot.on('message', (event) => {
+  if (data.courses.length === 0) {
+    event.reply('資料讀取中，請稍後再試')
+  } else if (event.message.type === 'text') {
+    if (event.message.text === '共通課程') {
+      data.replyCourses(event)
+    }
+  }
+})
+
+bot.listen('/', process.env.PORT || 3000, () => {
+  console.log('機器人啟動123456')
+})
---
+++ line/template.js
@@ -0,0 +1,26 @@
+export default {
+  type: 'bubble',
+  hero: {
+    type: 'image',
+    url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+    size: 'full',
+    aspectRatio: '1:1',
+    aspectMode: 'cover'
+  },
+  body: {
+    type: 'box',
+    layout: 'vertical',
+    contents: [
+      {
+        type: 'text',
+        text: 'Brown Cafe',
+        weight: 'bold',
+        size: 'xl'
+      },
+      {
+        type: 'text',
+        text: 'hello, world'
+      }
+    ]
+  }
+}
